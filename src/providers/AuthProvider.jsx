06ee12e593e687c5c8a25a1d6f263dcbcb4b3e24
e <jannatul0040@gmail.com>
2023-10-17 19:56:33 +0600

    added last login time

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 const auth = getAuth(app);
@@ -14,11 +14,15 @@ const AuthProvider = ({children}) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
     const userInfo = {
         user,
         loading,
-        createUser
-        // signInUser
+        createUser,
+        signInUser
     }
     return (
         <AuthContext.Provider value={userInfo}>
