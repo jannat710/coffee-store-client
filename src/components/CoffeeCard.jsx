1dbe5f338e6d3c765bfa588b8220de5f71b1d97b
e <jannatul0040@gmail.com>
2023-10-17 11:09:24 +0600

    created user

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 
-const CoffeeCard = ({coffee}) => {
-    const {_id,name, quantity, supplier, taste, category, details, photo}=coffee;
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+
+    const { _id, name, quantity, supplier, taste, photo } = coffee;
 
     const handleDelete = _id => {
         console.log(_id);
@@ -31,34 +32,38 @@ const CoffeeCard = ({coffee}) => {
                                 'Your Coffee has been deleted.',
                                 'success'
                             )
-                            
+                            const remaining = coffees.filter(cof => cof._id !== _id);
+                            setCoffees(remaining);
                         }
                     })
-                }
-            })
 
+            }
+        })
     }
-
 
     return (
         <div className="card card-side bg-base-100 shadow-xl">
-        <figure><img src={photo} alt="Movie"/></figure>
-        <div className="flex justify-between w-full pr-4">
-          <div>
-          <h2 className="card-title">Name : {name}</h2>
-          <p>{quantity}</p>
-          <p>{supplier}</p>
-          <p>{taste}</p>
-          </div>
-          <div className="card-actions justify-end">
-          <div className="btn-group btn-group-vertical space-y-4">
-  <button className="btn">View</button>
-  <Link to={`updateCoffee/${_id}`}><button className="btn">Edit</button></Link>
-  <button onClick={() => handleDelete(_id)} className="btn bg-orange-500">Delete</button>
-</div>
-          </div>
+            <figure><img src={photo} alt="Movie" /></figure>
+            <div className="flex justify-between w-full pr-4">
+                <div>
+                    <h2 className="card-title">Name: {name}</h2>
+                    <p>{quantity}</p>
+                    <p>{supplier}</p>
+                    <p>{taste}</p>
+                </div>
+                <div className="card-actions justify-end">
+                    <div className="btn-group btn-group-vertical space-y-4">
+                        <button className="btn">View</button>
+                        <Link to={`updateCoffee/${_id}`}>
+                        <button className="btn">Edit</button>
+                        </Link>
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn bg-orange-500">X</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     );
 };
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBI2Q8mgZFdXziE__j0kzEDFlCl1LoOs-M",
+  authDomain: "coffee-store-66829.firebaseapp.com",
+  projectId: "coffee-store-66829",
+  storageBucket: "coffee-store-66829.appspot.com",
+  messagingSenderId: "582671938749",
+  appId: "1:582671938749:web:12bf5f39e1dce9423833d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
